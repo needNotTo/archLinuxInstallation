9d09d2d2c971df92d0a5b00276209234b561809d
needNotTo <dibahmad09@gmail.com>
2021-01-16 01:49:55 +0000

    added sleep 2

--- archLinuxInstallation1.sh
+++ archLinuxInstallation1.sh
@@ -5,23 +5,35 @@ echo german keybordLayout
 
 echo connect to network
     iwctl
+    dd if=/dev/zero of=/dev/sda     <<enter the partition name
 COMMENT
 echo format disk
-    dd if=/dev/zero of=/dev/sda     <<enter the partition name
-    fdisk /dev/sda
         echo create 2 partition
             echo 300 MB for Kernel /dev/sda1
             echo remain for System /dev/sda2
+	    sleep 2
+    fdisk /dev/sda
+    sleep 2
     mkfs.ext4 /dev/sda1
     echo verschlüsselte partition
+    sleep 2
         cryptsetup -v -y --cipher aes-xts-plain64 --key-size 256 --hash sha256 --iter-time 2000 --use-urandom --verify-passphrase luksFormat /dev/sda2
+    sleep 2
     cryptsetup open /dev/sda2 SYSTEM
+    sleep 2
     mkfs.ext4 /dev/mapper/SYSTEM
 echo mount
+    sleep 2
     mount /dev/mapper/SYSTEM /mnt
+    sleep 2
     mkdir /mnt/boot
+    sleep 2
     mount /dev/sda1 /mnt/boot
+    sleep 2
 echo install system
+    sleep 2
     pacstrap /mnt base base-devel linux linux-firmware vim wpa_supplicant netctl dialog dhcpcd grub
+    sleep 2
     genfstab -Lp /mnt > /mnt/etc/fstab
+    sleep 2
     arch-chroot /mnt
